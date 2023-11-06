723f69f18bedf08f6df4c769e9bf5b47c0c0ae6c
chang an <Changan35@gmail.com>
2023-11-06 15:56:49 +0800

    login microservice

---
+++ login-microservice/recipes/views.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from .models import Favourites, Recipes
+from django.db.models import Count
+
+from .models import User, Recipes, Favourites
+from django.contrib import messages
+
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(("http://localhost:8001/"))
+        else:
+            return render(request, "recipes/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "recipes/login.html")
+
+
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "recipes/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "recipes/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(("http://localhost:8001/"))
+    else:
+        return render(request, "recipes/register.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(("http://localhost:8001/"))
+
