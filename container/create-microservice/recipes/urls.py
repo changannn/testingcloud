ff208650dc3816773db307beb8e37836212059b5
yyueda <yyuedaa@gmail.com>
2023-11-09 22:36:37 +0800

    Fix bugs

--- container/create-microservice/recipes/urls.py
+++ container/create-microservice/recipes/urls.py
@@ -3,9 +3,12 @@ from . import views
 
 app_name = "recipes"
 urlpatterns = [
+    path("", views.index, name="index"),
     path("create", views.create, name="create"),
     path("myrecipes", views.my_recipes, name="my_recipes"),
     path("myrecipes/update/<int:recipe_id>", views.update_recipe, name="update_recipe"),
     path("myrecipes/delete/<int:recipe_id>", views.delete_recipe, name="delete_recipe"),
     path("recipe/<int:recipe_id>", views.view_recipe, name="view_recipe"),
+    path("add_to_favourites/<int:recipe_id>", views.add_to_favourites, name="add_to_favourites"),
+    path('remove_from_favourites/<int:recipe_id>/', views.remove_from_favourites, name='remove_from_favourites'),
 ]
--- container/create-microservice/recipes/views.py
+++ container/create-microservice/recipes/views.py
@@ -12,6 +12,57 @@ from django.db.models import Count
 from .models import User, Recipes, Favourites
 from django.contrib import messages
 
+def index(request):
+    if request.user.is_authenticated:
+        user_favorites = Favourites.objects.filter(owner=request.user)
+        favorite_recipes = [fav.title for fav in user_favorites]
+    else:
+        favorite_recipes = []  # Initialize as an empty list if the user is not authenticated.
+
+    return render(request, "recipes/index.html", {
+        "recipes": Recipes.objects.all(),
+        "favorite_recipes": favorite_recipes,
+    })
+
+@login_required
+def add_to_favourites(request, recipe_id):
+    user = request.user
+    recipe = get_object_or_404(Recipes, pk=recipe_id)
+
+    # Check if the recipe is already in favorites
+    if Favourites.objects.filter(owner=user, title=recipe).exists():
+        messages.error(request, "Recipe is already in your favorites.")
+    else:
+        # Add the recipe to favorites
+        Favourites.objects.create(owner=user, title=recipe)
+
+        # Increment the likes count for the recipe
+        recipe.likes += 1
+        recipe.save()
+
+        messages.success(request, "Recipe added to favorites successfully.")
+
+    return redirect('recipes:index')  # Redirect to your index page
+
+@login_required
+def remove_from_favourites(request, recipe_id):
+    user = request.user
+    recipe = get_object_or_404(Recipes, pk=recipe_id)
+
+    try:
+        favorite = Favourites.objects.get(owner=user, title=recipe)
+        favorite.delete()
+
+        # Decrement the likes count for the recipe
+        if recipe.likes > 0:
+            recipe.likes -= 1
+            recipe.save()
+
+        messages.success(request, "Recipe removed from favorites successfully.")
+    except Favourites.DoesNotExist:
+        messages.error(request, "Recipe is not in your favorites.")
+
+    return redirect('recipes:index')  # Redirect to your index page
 
 def view_recipe(request, recipe_id):
     recipe = Recipes.objects.get(id=recipe_id)
@@ -46,13 +97,14 @@ def create(request):
         )
         recipe.save()
 
-        user_favorites = Favourites.objects.filter(owner=request.user)
-        favorite_recipes = [fav.title for fav in user_favorites]
+        # user_favorites = Favourites.objects.filter(owner=request.user)
+        # favorite_recipes = [fav.title for fav in user_favorites]
 
-        return render(request, "recipes/index.html", {
-            "recipes": Recipes.objects.all(),
-            "favorite_recipes": favorite_recipes,
-        })
+        # return render(request, "recipes/index.html", {
+        #     "recipes": Recipes.objects.all(),
+        #     "favorite_recipes": favorite_recipes,
+        # })
+        return HttpResponseRedirect(reverse("recipes:index"))
     else:
         return render(request, "recipes/create.html")
 
